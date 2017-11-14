ab26dcb7e20c217f50b1cbf2269bc0d53195ccbb
Darnell Arford <ugadarnell@gmail.com>
2017-11-14 16:12:51 -0500

    Add internationalization to Hello component

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -9,10 +9,21 @@ const styles = {
 };
 
 class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { lang: 'en' };
+  }
+  toggleLanguage() {
+    this.setState({ lang: (this.state.lang === 'sp' ? 'en' : 'sp') });
+  }
   render() {
     return (
       <div style={ styles.helloContainer }>
-        <h1 className="App-title">Hello, { this.props.username }!</h1>
+        <h1 className="App-title">{this.state.lang === 'sp'
+          ? `¡Hola, ${this.props.username}!`
+          : `Hello, ${this.props.username}!`}</h1>
+        <button onClick={ () => { this.toggleLanguage() } }>
+          {this.state.lang === 'sp' ? 'A Inglés' : 'To Spanish'}</button>
       </div>
     );
   }
