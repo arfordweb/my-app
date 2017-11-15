db8e1e0941760de26f9ce5237e422f6365503c14
Darnell Arford <ugadarnell@gmail.com>
2017-11-15 11:24:22 -0500

    Add UserTable component to demonstrate mapping over data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import Hello from './Hello';
+import UserTable from './UserTable';
 import './App.css';
 
 const logo = "https://www.sellerlabs.com/wp-content/uploads/sites/6/2017/02/SellerLabs_logo.png";
+const users = [
+  {username: 'Darnell', password: 'nqfy4&HD1$Cg95cp', role: 'admin'},
+  {username: 'Jimbob', password: 'passwurd', role: 'standard'},
+  {username: 'Jenny', password: '8675309', role: 'editor'},
+];
 
 class App extends Component {
   render() {
@@ -12,9 +18,7 @@ class App extends Component {
           <img src={logo} alt="logo" />
           <Hello username="Darnell" />
         </header>
-        <p className="App-intro">
-          Continue editing <code>src/App.js</code> and save to reload.
-        </p>
+        <UserTable users={ users } />
       </div>
     );
   }
---
+++ src/UserTable/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class UserTable extends Component {
+  render() {
+    return (<table className="user-table">
+      <thead>
+        <tr><th>User</th><th>Password</th><th>Role</th></tr>
+      </thead>
+      <tbody>
+        { this.props.users && this.props.users.map(function (user) {
+          return (<tr>
+            <td>{user.username}</td><td>{user.password}</td><td>{user.role}</td>
+          </tr>);
+        }) }
+      </tbody>
+    </table>);
+  }
+}
+
+export default UserTable;
